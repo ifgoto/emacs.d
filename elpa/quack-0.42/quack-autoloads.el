acb503dbc32d2273bcef6c7f8c4d0437199321d4
ifgoto <twitter9mail@mail.com>
2014-08-06 23:56:09 +0800

    add the elpa

---
+++ elpa/quack-0.42/quack-autoloads.el
@@ -0,0 +1,18 @@
+;;; quack-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("quack-pkg.el" "quack.el") (21312 65125
+;;;;;;  130462 972000))
+
+;;;***
+
+(provide 'quack-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; quack-autoloads.el ends here
---
+++ elpa/wxwidgets-help-0.0.3/wxwidgets-help-autoloads.el
@@ -0,0 +1,45 @@
+;;; wxwidgets-help-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads (wxhelp-browse-class-or-api wxhelp-browse-api wxhelp-class-index
+;;;;;;  wxhelp-api-index) "wxwidgets-help" "wxwidgets-help.el" (21312
+;;;;;;  64097 885919 979000))
+;;; Generated autoloads from wxwidgets-help.el
+
+(autoload 'wxhelp-api-index "wxwidgets-help" "\
+List wxWidgets API in its default HTML manual
+
+\(fn)" t nil)
+
+(autoload 'wxhelp-class-index "wxwidgets-help" "\
+List wxWidgets class in its default HTML manual
+
+\(fn)" t nil)
+
+(autoload 'wxhelp-browse-api "wxwidgets-help" "\
+
+
+\(fn K)" t nil)
+
+(autoload 'wxhelp-browse-class-or-api "wxwidgets-help" "\
+
+
+\(fn K)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("wxwidgets-help-pkg.el") (21312 64097
+;;;;;;  910648 961000))
+
+;;;***
+
+(provide 'wxwidgets-help-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; wxwidgets-help-autoloads.el ends here
